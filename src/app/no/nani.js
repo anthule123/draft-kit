8417ae4e98f7b77c7293e0eda531c229764a7f4f
an-yellow <raumuongxao234@tutamail.com>
2024-12-20 21:40:49 +0700

    fix

--- src/app/no/nani.js
+++
@@ -1,5 +0,0 @@
-
-import { $typst } from '@myriaddreamin/typst.ts/dist/esm/contrib/snippet.mjs';
-const mainContent = 'Hello, typst!';
-
-console.log(await $typst.svg({ mainContent }));
--- src/app/no/nani.mjs
+++
@@ -1,7 +0,0 @@
-
-import { NodeCompiler, DynLayoutCompiler } from '@myriaddreamin/typst-ts-node-compiler';
-
-const compiler = NodeCompiler.create();
-await compiler.pdf({
-    mainFileContent: 'Hello, typst!',
-  }); // :-> PDF Buffer
